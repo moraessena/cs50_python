3c42de454e1893778992d4ca801b6219b938cb76
daniel.sena <daniel.sena@tds.company>
2024-03-31 11:18:56 -0300

    correct capitalization

--- hello_world.py
+++ hello_world.py
@@ -5,5 +5,5 @@ this is also a comment
 name: str = input("what's your name? ") # variable 
 #print("hello, " + name) # prints a string concatenation
 #print("hello,", name) # prints the variable as an argument
-name = name.capitalize() # capitalize string
+name = name.title().strip() # capitalize string and strip whitespaces
 print(f"hello, {name}") # string interpolation
