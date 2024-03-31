945b5077c7cd59db642925304ffc1deea21cb9bd
daniel.sena <daniel.sena@tds.company>
2024-03-31 10:34:46 -0300

    functions and variables

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+"""
+this is also a comment
+"""
+# this is a comment
+name: str = input("what's your name? ") # variable 
+#print("hello, " + name) # prints a string concatenation
+#print("hello,", name) # prints the variable as an argument
+name = name.capitalize() # capitalize string
+print(f"hello, {name}") # string interpolation
