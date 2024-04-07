0bf0c3dc02926afd9c869df6d654241e07a279a3
dmms <danielsena04@gmail.com>
2024-04-07 16:22:44 -0300

    correct way of defining functions

--- functions.py
+++ functions.py
@@ -1,6 +1,9 @@
+def main():
+    name = input("what's your name? ")
+    hello(name)
+
+
 def hello(to="world"):
     print("hello,", to)
 
-hello()
-name = input("what's your name? ")
-hello(name)
+main()
