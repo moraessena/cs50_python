46e0174b83610c6db0a02d0dae6c2142d2db46e4
dmms <danielsena04@gmail.com>
2024-04-07 17:12:20 -0300

    create files

---
+++ compare.py
@@ -0,0 +1,22 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# v1
+# if x < y:
+#     print("x is less thant y")
+# elif x > y:
+#     print("x is greater than y")
+# else:
+#     print("x is equal to y")
+
+# v2
+# if x < y or x > y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+# v3
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
