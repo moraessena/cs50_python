735393f0aebee1a44468365871cf8e2fddee36b5
dmms <danielsena04@gmail.com>
2024-04-10 20:38:28 -0300

    use match instead of if-elif-else

--- house.py
+++ house.py
@@ -1,8 +1,20 @@
 name = input("Whats's your name? ")
 
-if name == "Harry" or name == "Hermione" or name == "Ron":
-    print("Gryffindor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Who?")
+# if name == "Harry" or name == "Hermione" or name == "Ron":
+#     print("Gryffindor")
+# elif name == "Draco":
+#     print("Slytherin")
+# else:
+#     print("Who?")
+
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who??")
