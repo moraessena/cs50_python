e3143a0f516a406ae6aabb322217165f39cc6626
dmms <danielsena04@gmail.com>
2024-04-10 21:22:51 -0300

    print meow 3x with python magic

--- 03_loops/cat.py
+++ 03_loops/cat.py
@@ -2,7 +2,7 @@
 # while i != 0:
 #     print("meow")
 #     i -= 1
-#
+
 # i = 0
 # while i < 3:
 #     print("meow")
@@ -14,5 +14,7 @@
 # for i in range(0, 3):
 #     print(f"{i} meow")
 
-for _ in range(0, 3):
-    print("meow")
+# for _ in range(0, 3):
+#     print("meow")
+
+print("meow\n" * 3)
