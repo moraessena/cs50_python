a96a373561ff3116b680f5a38fa908ff96740c64
dmms <danielsena04@gmail.com>
2024-04-14 16:39:09 -0300

    final way of printing meow

--- 03_loops/cat.py
+++ 03_loops/cat.py
@@ -1,27 +1,18 @@
-# i = 3
-# while i != 0:
-#     print("meow")
-#     i -= 1
+def main():
+    number = get_number()
+    meow(number)
 
-# i = 0
-# while i < 3:
-#     print("meow")
-#     i += 1
 
-# for i in [0, 1, 2]:
-#     print("meow")
+def get_number():
+    while True:
+        n = int(input("What's n?? "))
+        if n > 0:
+            return n
 
-# for i in range(0, 3):
-#     print(f"{i} meow")
 
-# for _ in range(0, 3):
-#     print("meow")
+def meow(n):
+    for _ in range(n):
+        print("meow")
 
-# print("meow\n" * 3, end="")
 
-while True:
-    n = int(input("What's n? "))
-    if n > 0:
-        break
-for _ in range(n):
-    print("meow")
+main()
